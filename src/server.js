c618dd32b29e9696bc07ad6f888373e0fadcfcd8
Enrique Perez <28enriqueperez@gmail.com>
2021-03-07 09:23:58 -0400

    feat: rename routes/user.js to routes/userRoutes.js for more consistency

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connectDB = require("./config/db");
-const usersRoutes = require("./routes/users");
+const usersRoutes = require("./routes/usersRoutes");
 const errorHandler = require("./middlewares/errorHandler");
 
 //configuration and middlewares
