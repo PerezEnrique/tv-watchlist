26e088b64103ed37c3a6a447593193ea6be3feaf
Enrique Perez <28enriqueperez@gmail.com>
2021-04-08 08:50:46 -0400

    feat: Implements Not Found Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { AuthenticationRoute } from "./utils/RoutesHandlers";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
@@ -7,6 +7,7 @@ import SignUpPage from "./pages/SignUpPage";
 import LogInPage from "./pages/LogInPage";
 import LogOutPage from "./pages/LogOutPage";
 import LoadingPage from "./pages/LoadingPage";
+import PageNotFound from "./pages/PageNotFound";
 import "./App.css";
 
 class App extends React.Component {
@@ -16,10 +17,12 @@ class App extends React.Component {
 				<Header />
 				<Switch>
 					<Route path="/loading-page" component={LoadingPage} />
-					<AuthenticationRoute path="/log-in" component={LogInPage} />
 					<AuthenticationRoute path="/sign-up" component={SignUpPage} />
+					<Route path="/not-found" component={PageNotFound} />
+					<AuthenticationRoute path="/log-in" component={LogInPage} />
 					<Route path="/log-out" component={LogOutPage} />
 					<Route path="/" exact component={HomePage} />
+					<Redirect to="/not-found" />
 				</Switch>
 			</div>
 		);
---
+++ client/src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class PageNotFound extends React.Component {
+	render() {
+		return (
+			<main className="main-container" role="main">
+				<h1>Sorry, page not found</h1>
+			</main>
+		);
+	}
+}
+
+export default PageNotFound;
